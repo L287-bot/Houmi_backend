5155bd61dce8b5cabc09241b09f733f9c464d80a
lschhh <2094676344@qq.com>
2024-09-24 17:58:23 +0800

    第一次

---
+++ src/main/java/com/example/houmi_backend/HoumiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.houmi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoumiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoumiBackendApplication.class, args);
+    }
+
+}
